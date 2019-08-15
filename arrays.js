ee13a21c1a82f2134a9fe80ed1c48346d2e4a014
Barak Saidoff <barak.saidoff@gmail.com>
2019-08-15 14:05:52 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,29 +1,21 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
-  return [element, ...array];
+
   return [element, ...array]
 }
 
-function descructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element);
-  return array;
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
   return array
 }
 
-function addElementToEndOfArray(array, element)  {
-  return [...array, element];
 function addElementToEndOfArray(array, element) {
   return [...array, element]
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  array.push(element);
-  return array;
   array.push(element)
   return array
 }
