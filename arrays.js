cbe05d4392b4f8d635bb360f0bb31e8084dd4506
Barak Saidoff <barak.saidoff@gmail.com>
2019-08-15 14:02:51 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, object) {
-  return array.splice(0,1,object)
+  return array.concat(object, ...array)
 }
 function destructivelyAddElementToBeginningOfArray(array, object) {
   return array.unshift(object)
